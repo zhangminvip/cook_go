05f6ada566e4520758c873113edf4981acdad73a
minzhang <a991391009@gmail.com>
2019-03-07 18:03:18 +0800

    output the index and value of parameter

--- src/test/ch1/echo2.go
+++ src/test/ch1/echo2.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
+	// "strings"
+	"strconv"
 )
 
 func main(){
-	// s, sep := "", ""
-	// for _, arg := range os.Args[1:]{
-	// 	s += sep + arg
-	// 	sep = " "
-	// }
+	s, sep := "", ""
+	for i, arg := range os.Args[1:]{
+		s += strconv.Itoa(i) + ". " + sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
 	// fmt.Println(os.Args[1:])
-	fmt.Println(strings.Join(os.Args[1:], " "))
+	// fmt.Println(strings.Join(os.Args[1:], " "))
 }
